d9caa03c31ca19ce5e5a9ba9b6fe9fe2326e3615
YOHAN ALEK PLAZOLA ARANGURE <2021030101@upsin.edu.mx>
2024-02-26 20:52:43 -0700

    Se terminó la página de login

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,10 +1,15 @@
 const btnMenu = document.querySelector('.menu');
 const btnExit = document.querySelector('.exit');
+const inputs = document.querySelectorAll('input');
+const inputsIcons = document.querySelectorAll('.input > i');
+
 const background = document.querySelector('.background');
 const buttons = document.querySelector('.buttons');
 
 btnMenu.addEventListener('click', e => {
     if(!buttons.classList.contains('active')) {
+        inputs.forEach(input => input.style.zIndex = -1);
+        inputsIcons.forEach(icon => icon.style.zIndex = -1);
         buttons.classList.add('active');
         background.classList.add('active');
     }
@@ -12,6 +17,8 @@ btnMenu.addEventListener('click', e => {
 
 btnExit.addEventListener('click', e => {
     if(buttons.classList.contains('active')){
+        inputs.forEach(input => input.style.zIndex = 1);
+        inputsIcons.forEach(icon => icon.style.zIndex = 1);
         buttons.classList.remove('active');
         background.classList.remove('active');
     }
