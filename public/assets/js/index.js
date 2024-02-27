06633cac795d57d413cb2af9743c0f1dc6dd2a10
YOHAN ALEK PLAZOLA ARANGURE <2021030101@upsin.edu.mx>
2024-02-26 19:40:31 -0700

    Se terminó el head de la página

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,1 +1,18 @@
-console.log('index.html');
+const btnMenu = document.querySelector('.menu');
+const btnExit = document.querySelector('.exit');
+const background = document.querySelector('.background');
+const buttons = document.querySelector('.buttons');
+
+btnMenu.addEventListener('click', e => {
+    if(!buttons.classList.contains('active')) {
+        buttons.classList.add('active');
+        background.classList.add('active');
+    }
+});
+
+btnExit.addEventListener('click', e => {
+    if(buttons.classList.contains('active')){
+        buttons.classList.remove('active');
+        background.classList.remove('active');
+    }
+});
